83b43ebe7e58dc6fe0ef5e307947c523dc07805b
mnoqc <nmomnr@gmail.com>
2025-09-02 22:17:18 +0900

    Initial commit

---
+++ src/main/java/com/mnoqc/rollscheduler/RollSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.mnoqc.rollscheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RollSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RollSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mnoqc/rollscheduler/RollSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mnoqc.rollscheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RollSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
